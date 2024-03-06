3409ee75fb4e0878177696992c96bc902acd26c9
buddhadeb <jeetkoner36@gmail.com>
2024-03-06 10:23:04 +0530

    list

--- apna collage/list & tupple/list_methods.py
+++ apna collage/list & tupple/list_methods.py
@@ -1,10 +1,6 @@
 # Create an empty list
 my_list = []
 
-# append(), extend(), insert(), remove(), pop(), clear(), sort(), and reverse()
-# above function will return null value so.
-# i will change the original list.
-
 # Append an item to the end of the list
 my_list.append(5)
 print("After appending:", my_list)
@@ -14,24 +10,28 @@ my_list.extend([6, 7, 8])
 print("After extending:", my_list)
 
 # Insert an item at a specified position
-my_list.insert(1, 10)
+my_list.insert(1, 10)  # Two arguments: index and element
 print("After inserting:", my_list)
 
 # Remove the first occurrence of an item from the list
-my_list.remove(6)
+my_list.remove(6)  # To remove element 6
 print("After removing:", my_list)
 
 # Remove and return the item at the specified position
-popped_item = my_list.pop(2)
+popped_item = my_list.pop(2)  # One argument: index of the list
+# It will return which element was popped
+
 print("Popped item:", popped_item)
 print("After popping:", my_list)
+new_list = my_list
 
 # Clear all items from the list
 my_list.clear()
 print("After clearing:", my_list)
 
 # Index of the first occurrence of an item in the list
-if my_list:
+my_list = new_list
+if new_list:
     index = my_list.index(5)
     print("Index of 5:", index)
 else:
@@ -44,15 +44,17 @@ if my_list:
 else:
     print("List is empty.")
 
-
-# Sort the items of the list in place
-my_list = [3, 1, 4, 1, 5, 9, 2]
-my_list.sort()
-print("After sorting:", my_list)
-
 # Reverse the elements of the list in place
 my_list.reverse()
 print("After reversing:", my_list)
+
+# Sort the items of the list in place
+# Not necessary it will always be numbers
+my_list = [3, 1, 4, 1, 5, 9, 2]  # Should be strings
+my_list.sort()  # Sorts in ascending order
+print("After ascending sorting:", my_list)
+my_list.sort(reverse=True)  # Sorts in descending order
+print("After descending sorting:", my_list)
 
 # Shallow copy of the list
 copied_list = my_list.copy()
