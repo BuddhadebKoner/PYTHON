159acf2fbd47e49dce2bf46be939e1f6c669613e
buddhadeb <jeetkoner36@gmail.com>
2024-03-13 23:41:41 +0530

    currection

--- Zero to hero/opps/attribute.py
+++ Zero to hero/opps/attribute.py
@@ -1,36 +1,33 @@
-'''
-What will be common should be declared as a class attribute.
 
-For example, in a student class, the college name is the same for every student, so it's stored as a class attribute to avoid redundancy.
+''' wchich will be common that will be a class attribute 
+'''
+
+'''
+for example i have student class , there collage name is same for every student so i will not store the collage name again and again , one time i will declear it in the class atribute 
 '''
 
 
 class Student:
-    # College name is the same for all students
-    college_name = "Sanaka Educational Trust"  # one time it will store in the memory
+    # collage name is same for all students
+    collage_name = "Sanaka Educational Trust" # one time it will store in thr mrmory
     
-    # Default name if not provided
-    default_name = "Anonymous"
-
-    # Class attribute
-    name = default_name  
-
-    def __init__(self, name=None, mark=None):
-        if name:
-            self.name = name  # Object attribute
-        else:
-            self.name = self.__class__.name  # Using class attribute as default name
+    name = "Anonomus" # class attr 
+    
+    def __init__(self,name,mark):
+        self.name = name # obj attr
         self.mark = mark
+        print("Adding the student details in the storage")
+        
+s1 = Student("Buddhadeb",100)
+print(s1.name,s1.mark)
 
-# Creating instances of students
-s1 = Student("Buddhadeb", 100)
-s2 = Student("Jeet", 40)
-s3 = Student(mark=70)  # Providing mark only, name will be default
-s4 = Student()  # Neither name nor mark provided, both will be default
+s2 = Student("Jeet",40)
+print(s2.name,s2.mark)
 
-# Output
-print(s1.name, s1.mark)
-print(s2.name, s2.mark)
-print(s3.name, s3.mark)
-print(s4.name)  # Only name, mark not provided, will be default
-print(s2.college_name)  # Accessing class attribute
+print(s2.collage_name) # this is class attribute
+
+print(s1.name) # if have , always obj attr have higher precedence
+# class attr << obj attr
+'''
+use case : if any student dont give name then the class attr will be the default value
+'''
