10e29066cb3ae7194e6c7109753efb42083e626e
buddhadeb <jeetkoner36@gmail.com>
2024-03-16 14:10:01 +0530

    attribute

--- Zero to hero/opps/attribute.py
+++ Zero to hero/opps/attribute.py
@@ -5,7 +5,6 @@
 for example i have student class , there collage name is same for every student so i will not store the collage name again and again , one time i will declear it in the class atribute 
 '''
 
-
 class Student:
     # collage name is same for all students
     collage_name = "Sanaka Educational Trust" # one time it will store in thr mrmory
@@ -30,3 +29,29 @@ print(s1.name) # if have , always obj attr have higher precedence
 '''
 use case : if any student dont give name then the class attr will be the default value
 '''
+
+
+
+
+'''
+Question : - program modifying a mutable type attribute
+'''
+class Number:
+    even = [] # class attr
+    oods = []
+    def __init__(self,num):
+        self.num = num
+        if num % 2 == 0:
+            Number.even.append(num)
+        else:
+            Number.oods.append(num)
+            
+            
+n1 = Number(21)
+n1 = Number(32)
+n1 = Number(43)
+n1 = Number(54)
+n1 = Number(65)
+
+print("Even Numbers are : ",Number.even)
+print("Odd Numbers are : ",Number.oods)
